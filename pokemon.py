aab7fda1a301dc9cc4f4626afee2555a9d99782a
binhm <44992002+binhm@users.noreply.github.com>
2019-08-05 19:33:20 -0700

    changed using vm

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,7 @@
-from lib import pygame
-from include import testingclass
+import pygame
 
 if __name__ == '__main__':
     print("main file")
     print("testing")
-    # include.testingclass.practice()
-    # testingclass.practice()
-    testingclass.practice()
+    # pygame.init()
+    
