fec3883cdefb7b620bf2a79ac11e721b1be6e7ea
binhm <44992002+binhm@users.noreply.github.com>
2019-08-05 17:45:51 -0700

    updated organization

---
+++ pokemon.py
@@ -0,0 +1,9 @@
+from lib import pygame
+from include import testingclass
+
+if __name__ == '__main__':
+    print("main file")
+    print("testing")
+    # include.testingclass.practice()
+    # testingclass.practice()
+    testingclass.practice()
