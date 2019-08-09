41d27d13290da4d2de03402c8285434c2ab9261b
binhm <44992002+binhm@users.noreply.github.com>
2019-08-09 13:02:13 -0700

    user can choose different avatar from 1-5

--- player.py
+++ player.py
@@ -8,16 +8,59 @@ class Player:
 		self.height = 50
 		self.velocity = 10
 		self._avatarcell = 0
-		self._cell = 3 ## hard coded to 3 right now
-	def avatarcell(self):
+		self._current_row = 0
+		self._current_col = 0
+		self.path = ''
+		self._spriter = 0
+		self._spritec = 0
+		self._area = 0
+		# self._track_row = 1
+		self._avatar = 0
+	def avatar(self, num: int)-> 'path':
+		'''player needs to choose avatar, 
+			numbered from 1 and return a string path'''
+		self._avatar = num
+		self.spritesheet()
+		return self.path
+
+	def current_row(self) -> int:
 		'''
 		Returns the cell of avatar movement and which one should be 
 		drawn on screen.
 		'''
-		### Not accounting for pictures with multilples rows.
-		return self._avatarcell
+		return self._current_row
+	def current_col(self)->int:
+		return self._current_col
 
+	def sprite_row(self):
+		''' returns the # of row of the spritesheet'''
+		return self._spriter
+	def sprite_col(self):
+		''' returns the # of row of the spritesheet'''
+		return self._spritec
+	def sprite_total_cell(self):
+		''' total number of cells in the sprite sheet '''
+		return self._spriter * self._spritec
+	def sprite_area(self, width: int, height: int):
+		'''returns the sprite cell area given a width and height'''
+		pass
+	def spritesheet(self):
+		'''
+		returns a list of avatar movements
+		'''
+		if self._avatar <= 2:
+			# avatar with different row and col
+			
+			self._spriter = 1
+			self._spritec = 4
+			self.path = 'backgrounds/character1/bd.png' if self._avatar == 1 else 'backgrounds/character1/gd.png' 
+			
+		else:
+			# other avatars
 
+			self._spriter = 4
+			self._spritec = 3
+			self.path = 'backgrounds/avatars/' + str(self._avatar) + '.png'
 
 
 
@@ -27,15 +70,39 @@ class Player:
 	if the avatar is outside, because the background will be the one
 	moving not the avatar itself.
 	'''
-	def moveup(self):
-		self.y -= self.velocity
-		self._avatarcell += 1 if self._avatarcell < self._cell else -self._cell
 	def movedown(self):
+		
+		# self._track_row = 1
+		# if self.avatarcell > 2:
+		# 	'''if not avatar 1, 2 then move down starts at row 1'''
+		# 	# self.avatarcell = 0
+		# 	self._track_row = 1
+		self._current_row = 0
+		self.path = 'backgrounds/character1/bd.png' if self._avatar == 1 else 'backgrounds/character1/gd.png' 
+		self._current_col += 1 if self._current_col < self.sprite_col()-1 else -self.sprite_col()+1
 		self.y += self.velocity
-		self._avatarcell += 1 if self._avatarcell < self._cell else -self._cell
-	def moveright(self):
-		self.x += self.velocity
-		self._avatarcell += 1 if self._avatarcell < self._cell else -self._cell
 	def moveleft(self):
+		
+		# if self._avatar > 2:
+		# 	'''if not avatar 1, 2 then move down starts at row 2'''
+		# 	self._track_row = 2
+		self._current_row = 0 if self._avatar <= 2 else 1
+		self.path = 'backgrounds/character1/bl.png' if self._avatar == 1 else 'backgrounds/character1/gl.png' 
+		self._current_col += 1 if self._current_col < self.sprite_col()-1 else -self.sprite_col()+1
 		self.x -= self.velocity
-		self._avatarcell += 1 if self._avatarcell < self._cell else -self._cell
+	def moveright(self):
+		
+		# if self._avatar > 2:
+		# 	'''if not avatar 1, 2 then move down starts at row 1'''
+		# 	self._track_row = 3
+		self._current_row = 0 if self._avatar <= 2 else 2
+		self.path = 'backgrounds/character1/br.png' if self._avatar == 1 else 'backgrounds/character1/gr.png' 
+		self._current_col += 1 if self._current_col < self.sprite_col()-1 else -self.sprite_col() + 1
+		self.x += self.velocity
+	def moveup(self):
+		# self.y -= self.velocity
+		self._current_row = 0 if self._avatar <= 2 else 3
+		self.path = 'backgrounds/character1/bu.png' if self._avatar == 1 else 'backgrounds/character1/gu.png' 
+
+		self._current_col += 1 if self._current_col < self.sprite_col() -1  else -self.sprite_col() +1
+		self.y -= self.velocity
--- pokemon.py
+++ pokemon.py
@@ -16,13 +16,26 @@ class Pokemon:
 		# self.avatar = None
 	def play(self):
 		'''if the game is in the playing state'''
+		self.player.avatar(4) ## CHOSE THE AVATAR from 1 - 5, Can put somewhere else
+		self.avatar = pygame.image.load(self.player.path).convert_alpha() # 
+
+		# do the calculations of where to get the sprite movements
+		area = self.avatar.get_width(), self.avatar.get_height() # get the dimension of the entire image
+		sprite_width, sprite_height = area[0]/self.player.sprite_col(), area[1]/self.player.sprite_row() 
 		
-		self.avatar = pygame.image.load('backgrounds/character1/d.png').convert_alpha()
 		while self._running:
 			# print("Game is running")
+
+			if self.player._avatar <= 2:
+				## since if we choose avatar 1, 2 then 
+				## we are loading different pictures since movements are croped
+				## to different pictures
+				self.avatar = pygame.image.load(self.player.path).convert_alpha()
+
 			pygame.time.delay(100)
 			# Just checks to see if user clicked the "X" on the top
 			# right of the window
+
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					self.quit()
@@ -44,10 +57,12 @@ class Pokemon:
 
 			### make a draw class
 			self._window.fill((0, 0, 0)) 
-	
+			
+			
 			self._window.blit(self.avatar, 
 				(self.player.x, self.player.y), 
-				(self.player.avatarcell()*self.avatar.get_width()/4,0, self.avatar.get_width()/4, self.avatar.get_height()))
+				(self.player.current_col()*sprite_width,self.player.current_row() * sprite_height, 
+				sprite_width, sprite_height))
 			pygame.display.update()
 	# def handle_pause_key_events():
 
@@ -98,7 +113,10 @@ class Pokemon:
 		self.player = player.Player(600, 600)
 		pygame.display.set_caption('Pokemon') # Sets the window name 
 
-		
+		##---------------------------set up characters ------------------
+		## let player choose types of avatar
+		# self.player.avatar(1)
+		##--------------------------------------------------------------##
 		while not (self._running == False and self._pause == False):
 			
 			self.play()
@@ -126,19 +144,23 @@ class Pokemon:
 		'''
 		player movements
 		'''
+
 		if keys[pygame.K_UP]:
-			self.avatar = pygame.image.load('backgrounds/character1/u.png').convert_alpha()
+			# self.avatar = pygame.image.load('backgrounds/character1/gu.png').convert_alpha()
 			self.player.moveup()
+			# self.player.move('u')
 		if keys[pygame.K_DOWN]:
-			self.avatar = pygame.image.load('backgrounds/character1/d.png').convert_alpha()
+			# self.avatar = pygame.image.load('backgrounds/character1/gd.png').convert_alpha()
 			self.player.movedown()
+			# self.player.move('d')
 		if keys[pygame.K_RIGHT]:
-			self.avatar= pygame.image.load('backgrounds/character1/r.png').convert_alpha()
+			# self.avatar= pygame.image.load('backgrounds/character1/gr.png').convert_alpha()
 			self.player.moveright()
+			# self.player.move('r')
 		if keys[pygame.K_LEFT]:
-			self.avatar = pygame.image.load('backgrounds/character1/l.png').convert_alpha()
+			# self.avatar = pygame.image.load('backgrounds/character1/gl.png').convert_alpha()
 			self.player.moveleft()
-		
+			# self.player.move('l')
 
 		'''settings. Problem: holding any key shouldn't do anything'''
 		
@@ -146,6 +168,8 @@ class Pokemon:
 			print("Save option")
 			f = open("SAVED_GAME.txt", "w+")
 			### created a txt file for saved game, what what should be saved??
+
+			f.write("%s\n", self.player._avatar) # Save the avatar
 
 		if keys[pygame.K_ESCAPE]:
 			# If user press esc, then bring up the setting option
