66c8e790bcbcafba978975c22ce83bbe51eeb922
binhm <44992002+binhm@users.noreply.github.com>
2019-08-14 18:06:43 -0700

    add tiles, map

---
+++ backgrounds/Pokemon1-background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Pokemon1" tilewidth="32" tileheight="32" tilecount="1485" columns="45">
+ <image source="tileset-alistair.png" width="1461" height="1056"/>
+</tileset>
---
+++ map.py
@@ -0,0 +1,12 @@
+#pixel
+TILE_HEIGHT = 16
+TILE_WIDTH = 16
+
+class Map:
+	def __init__(self):
+		pass
+
+
+class camera:
+	def __init__(self, width, height):
+		pass
